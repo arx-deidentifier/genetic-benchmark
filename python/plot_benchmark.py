0443e47985a24410afcb490c7ba827877b0406b5
srcds <t.meurers@fu-berlin.de>
2020-09-23 15:18:19 +0200

    Added license header (python)

--- python/plot_benchmark.py
+++ python/plot_benchmark.py
@@ -1,3 +1,20 @@
+"""
+Benchmark of ARX's Heuristic Algorithms
+ Copyright 2020 by Thierry Meurers and contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
 import pandas as pd
 import itertools
 import numpy as np
